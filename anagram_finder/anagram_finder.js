8103c90a7de1a36c630cb7293c3d34010f3b8d99
Mark Payne <mjpayne8@gmail.com>
2019-10-16 16:27:46 +0100

    anagram method writte, tests passed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,25 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('').sort();
+  this.asString = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  return otherWords.filter(word => this.isAnagram(word));
 }
+
+AnagramFinder.prototype.isAnagram = function(anagramWord){
+  const wordArray = anagramWord.toLowerCase().split('').sort()
+  const anagramWordCount = wordArray.map(letter => this.letterCount(letter,wordArray));
+  const thisWordCount = this.word.map(letter => this.letterCount(letter,this.word));
+  const sameCount1 = anagramWordCount.every((number, index) => number === thisWordCount[index]);
+  const sameCount2 = thisWordCount.every((number,index) => number === anagramWordCount[index]);
+  const everyLetter = wordArray.every(letter => this.word.includes(letter));
+  const sameWord = this.asString === anagramWord;
+  return sameCount1 && sameCount2 && everyLetter && !sameWord;
+};
+
+AnagramFinder.prototype.letterCount = function(letter,wordArray){
+  return wordArray.filter(arrayLetter => arrayLetter === letter).length;
+};
 
 module.exports = AnagramFinder;
