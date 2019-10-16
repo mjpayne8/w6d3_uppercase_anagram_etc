f1b6d14423d837e52f41fb5caf41e94d15c2f3cd
Mark Payne <mjpayne8@gmail.com>
2019-10-16 15:52:28 +0100

    isogram method writte, tests passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,13 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  return this.word.every(letter => this.letterCount(letter,this.word)===1);
 }
+
+IsogramFinder.prototype.letterCount = function(letter,wordArray){
+  return wordArray.filter(arrayLetter => arrayLetter === letter).length;
+};
 
 module.exports = IsogramFinder;
