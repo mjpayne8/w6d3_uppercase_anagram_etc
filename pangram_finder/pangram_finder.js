fcba63acbb8ae4cdfda7d01f49bb9d685988c91f
Mark Payne <mjpayne8@gmail.com>
2019-10-16 15:33:30 +0100

    pangram tests passed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.reduce((accumalator,letter) => accumalator && this.phrase.includes(letter),true);
 }
 
 module.exports = PangramFinder;
